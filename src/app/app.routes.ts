19a71afb1d654b2cc2f56db490cf741b4cd64dff
¨jogeiryp¨ <jogeiryp@gmail.com>
2026-07-08 10:56:30 -0400

    git reset --hard HEAD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'bienvenida', pathMatch: 'full' },
   {
-    path: 'bienvenida',
-    loadComponent: () => import('./pages/bienvenida/bienvenida.page').then(m => m.BienvenidaPage)
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full'
   },
   {
-    path: 'tabs',
-    loadComponent: () => import('./tabs/tabs.component').then(m => m.TabsComponent),
-    children: [
-      { path: 'inicio', loadComponent: () => import('./pages/inicio/inicio.page').then(m => m.InicioPage) },
-      { path: 'agregar-gasto', loadComponent: () => import('./pages/agregar-gasto/agregar-gasto.page').then(m => m.AgregarGastoPage) },
-      { path: 'graficas', loadComponent: () => import('./pages/graficas/graficas.page').then(m => m.GraficasPage) },
-      { path: 'metas', loadComponent: () => import('./pages/metas/metas.page').then(m => m.MetasPage) },
-      { path: 'alertas', loadComponent: () => import('./pages/alertas/alertas.page').then(m => m.AlertasPage) },
-      { path: '', redirectTo: 'inicio', pathMatch: 'full' }
-    ]
-  },
-  { path: '**', redirectTo: 'bienvenida' },
-  {
-    path: 'bienvenida',
-    loadComponent: () => import('./pages/bienvenida/bienvenida.page').then( m => m.BienvenidaPage)
+    path: 'folder/:id',
+    loadComponent: () =>
+      import('./folder/folder.page').then(m => m.FolderPage)
   }
 ];
